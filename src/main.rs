878a7c65059054585b6d0c2027f076731853eca9
Elmar Athmer <elmar@athmer.org>
2023-08-29 22:37:08 +0200

    wait for wg interface to be available

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::env;
 use std::env::args;
+use std::time::Duration;
 
 use rtnetlink::new_connection;
+use tokio::time::sleep;
 
 use crate::dns::node_repository::DnsNodeRepository;
 use crate::mesh::wgmesh::WgMesh;
@@ -15,6 +17,20 @@ mod model;
 mod routing;
 mod traits;
 mod wireguard;
+
+pub async fn init_retry_wg(interface_name: &str) -> Result<WireguardImpl, error::Wireguard> {
+    for _ in 0..12 {
+        let wireguard = WireguardImpl::new(interface_name);
+
+        if wireguard.is_ok() {
+            return wireguard;
+        }
+
+        sleep(Duration::from_secs(5)).await;
+    }
+
+    WireguardImpl::new(interface_name)
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -30,8 +46,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let peer_repository = DnsNodeRepository::init(mesh_resolver)?;
 
-    // TODO: loop/wait for device to be available
-    let wireguard_device = WireguardImpl::new(&interface_name)?;
+    let wireguard_device = init_retry_wg(&interface_name).await?;
     let routing_service = RoutingServiceImpl::new(handle, &interface_name);
 
     let wg_mesh = WgMesh::new(peer_repository, routing_service, wireguard_device);
